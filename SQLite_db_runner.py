a3887c5d1320f63154bbcc8101e5a448aeafde2d
MrRuheeeee <mathishenkel@gmail.com>
2024-10-30 11:33:25 +0100

    jetzt ausführbar

--- SQLite_db_runner.py
+++ SQLite_db_runner.py
@@ -5,6 +5,8 @@ import sqlite3
 my_db = sqlite3.connect('users.db')
 my_dbc = my_db.cursor()
 
+#my_dbc.execute("""DROP TABLE roles""")
+#my_db.commit()
 
 my_dbc.execute("""CREATE TABLE roles (roles TEXT, can_see BOOLEAN, can_book BOOLEAN, can_add_item BOOLEAN, can_eddit_item BOOLEAN, can_delete_item BOOLEAN, can_eddit_categories BOOLEAN, can_eddit_roles BOOLEAN, can_eddit_user BOOLEAN)""")
 my_db.commit()
@@ -12,7 +14,7 @@ my_db.commit()
 my_dbc.execute("""INSERT INTO roles 
                (roles, can_see, can_book, can_add_item, can_eddit_item, can_delete_item, can_eddit_categories, can_eddit_roles, can_eddit_user)
                 VALUES 
-               ('Admin',1,1,1,1,1,1,1,1),
+               ('admin',1,1,1,1,1,1,1,1),
                ('user',1,1,0,0,0,0,0,0),
                ('guest',0,0,0,0,0,0,0,0)""")
 my_db.commit()
