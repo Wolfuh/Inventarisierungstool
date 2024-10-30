7c098553b98e46b202ac2b858e4ec9f992a2b551
MrRuheeeee <mathishenkel@gmail.com>
2024-10-30 11:27:40 +0100

    added guest, and looks better

--- SQLite_db_runner.py
+++ SQLite_db_runner.py
@@ -9,7 +9,10 @@ my_dbc = my_db.cursor()
 my_dbc.execute("""CREATE TABLE roles (roles TEXT, can_see BOOLEAN, can_book BOOLEAN, can_add_item BOOLEAN, can_eddit_item BOOLEAN, can_delete_item BOOLEAN, can_eddit_categories BOOLEAN, can_eddit_roles BOOLEAN, can_eddit_user BOOLEAN)""")
 my_db.commit()
 
-my_dbc.execute("""INSERT INTO roles (roles, can_see, can_book, can_add_item, can_eddit_item, can_delete_item, can_eddit_categories, can_eddit_roles, can_eddit_user) VALUES ('Admin',1,1,1,1,1,1,1,1)""")
-my_db.commit()
-my_dbc.execute("""INSERT INTO roles (roles, can_see, can_book, can_add_item, can_eddit_item, can_delete_item, can_eddit_categories, can_eddit_roles, can_eddit_user) VALUES ('user',1,1,0,0,0,0,0,0)""")
+my_dbc.execute("""INSERT INTO roles 
+               (roles, can_see, can_book, can_add_item, can_eddit_item, can_delete_item, can_eddit_categories, can_eddit_roles, can_eddit_user)
+                VALUES 
+               ('Admin',1,1,1,1,1,1,1,1),
+               ('user',1,1,0,0,0,0,0,0),
+               ('guest',0,0,0,0,0,0,0,0)""")
 my_db.commit()
