e9d3476cc1a6608729a0fcc68cadad6841623b9a
MrRuheeeee <mathishenkel@gmail.com>
2024-10-30 10:38:55 +0100

    kommentiert und etwas cleaner

--- SQLite_db.py
+++ SQLite_db.py
@@ -1,14 +1,16 @@
 import sqlite3
 
-my_db = sqlite3.connect('test.db')
+my_db = sqlite3.connect('users.db')
 my_dbc = my_db.cursor()
 
-#sql = """
-#        CREATE TABLE name(
-#            username TEXT,
-#            password TEXT
-#        )
-#    """
+
+#Tabelle erstellen: CREATE TABLE tabellenname (N_überschrift1 TEXT, N_überschrift2 BOOLEAN)
+#Daten einfügen:    INSTERT INTO tabellenname (überschrift1, überschrift2) VALUES (N_wert2 TEXT, N-wert2 BOOLEAN)
+#Daten ändern:      UPDATE tabellenname SET überschrift1 = N_wert1 WHERE bedingung 
+#   z.B. """UPDATE rollen2 SET add_user = 0 WHERE name = 'nutzer'"""
+#ausführen: my_dbc.execute(sql)
+#           my_db.commit()
+
 
 def addUser(name, passw):
     sql = f"""
@@ -20,10 +22,13 @@ def addUser(name, passw):
     my_dbc.execute(sql)
     my_db.commit()
 
+
 def get_all_data() -> list[tuple[str, str]]:  # type: ignore
     my_dbc.execute("SELECT * FROM name")
     nutzer = my_dbc.fetchall()
     return nutzer
+
+
 all_users = get_all_data()
 
 while True:
@@ -44,11 +49,12 @@ while True:
             print("abgebrochen")
             continue
 
-        BothIsKnown = any(user.lower() == existing_user.lower() and password == existing_password for (existing_user, existing_password) in all_users)
+        BothIsKnown = any(user.lower() == existing_user.lower() and password == existing_password for
+                          (existing_user, existing_password) in all_users)
 
         if BothIsKnown:
             print("erfolgreich eingeloggt")
-            logged_in = True
+            # logged_in = True
         else:
             print("benutzername oder passwort ist falsch")
 
@@ -70,6 +76,7 @@ while True:
                 break
             addUser(user, password)
             print("benutzer erfogreich hinzugefügt")
+            break
     elif action == '3':
         all_users = get_all_data()
         print(all_users)
