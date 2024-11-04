3fb960a843f099963a8b1baa7ee4ad650093e7fd
MrRuheeeee <mathishenkel@gmail.com>
2024-11-04 11:31:00 +0100

    tabelle wird nur erstellt, wenn nicht vorhanden

--- SQLite_db_runner.py
+++ SQLite_db_runner.py
@@ -5,8 +5,8 @@ import sqlite3
 my_db = sqlite3.connect('users.db')
 my_dbc = my_db.cursor()
 
-#my_dbc.execute("""DROP TABLE roles""")
-#my_db.commit()
+my_dbc.execute("""DROP TABLE IF EXISTS roles""")
+my_db.commit()
 
 my_dbc.execute("""CREATE TABLE roles (roles TEXT, can_see BOOLEAN, can_book BOOLEAN, can_add_item BOOLEAN, can_eddit_item BOOLEAN, can_delete_item BOOLEAN, can_eddit_categories BOOLEAN, can_eddit_roles BOOLEAN, can_eddit_user BOOLEAN)""")
 my_db.commit()
@@ -18,4 +18,3 @@ my_dbc.execute("""INSERT INTO roles
                ('user',1,1,0,0,0,0,0,0),
                ('guest',0,0,0,0,0,0,0,0)""")
 my_db.commit()
-#mrbeast
