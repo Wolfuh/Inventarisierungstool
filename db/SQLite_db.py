08ae6cc83850209db2c5805b20a451c656242caf
MrRuheeeee <mathishenkel@gmail.com>
2024-11-04 14:39:33 +0100

    auf users db angelehnt, nutzer können entfernt werden und Übersicht hinzugefügt

--- db/SQLite_db.py
+++ db/SQLite_db.py
@@ -1,34 +1,33 @@
 import sqlite3
 
-my_db = sqlite3.connect('test.db')
+my_db = sqlite3.connect('users.db')
 my_dbc = my_db.cursor()
 
-#sql = """
-#        CREATE TABLE rollen2(
-#            name TEXT,
-#            add_user BOOLEAN
-#        )
-#    """
-#my_dbc.execute(sql)
-#my_db.commit()
 
-sql = """ALTER TABLE rollen2
-ADD """
+#Tabelle erstellen: CREATE TABLE tabellenname (N_überschrift1 TEXT, N_überschrift2 BOOLEAN)
+#Daten einfügen:    INSERT INTO tabellenname (überschrift1, überschrift2) VALUES (N_wert2 TEXT, N-wert2 BOOLEAN)
+#Daten ändern:      UPDATE tabellenname SET überschrift1 = N_wert1 WHERE bedingung 
+#   z.B. """UPDATE rollen2 SET add_user = 0 WHERE name = 'nutzer'"""
+#ausführen: my_dbc.execute(sql)
+#           my_db.commit()
 
-def addUser(name, passw):
-    sql = f"""
-        INSERT INTO rollen2 (name, add_user) VALUES (
-            '{name}',
-            '{bool(passw)}'
-        )
-    """
-    my_dbc.execute(sql)
+
+def add_user(name, passw):
+    sql = "INSERT INTO users (user_first_name, password) VALUES (?, ?)"
+    my_dbc.execute(sql, (name,passw))
     my_db.commit()
 
-def get_all_data() -> list[tuple[str, str]]:  # type: ignore
-    my_dbc.execute("SELECT * FROM rollen2")
+def delete_user(zu_löschender_user):
+    sql = """DELETE FROM users WHERE user_first_name = ?"""
+    my_dbc.execute(sql, (zu_löschender_user,))
+    my_db.commit()
+
+def get_all_data() -> list[tuple]: # type: ignore
+    my_dbc.execute("SELECT * FROM users")
     nutzer = my_dbc.fetchall()
     return nutzer
+
+
 all_users = get_all_data()
 
 while True:
@@ -36,48 +35,60 @@ while True:
                    "1: Anmelden\n"
                    "2: Benutzer anlegen\n"
                    "3: datenbank ansehen\n"
+                   "4: Nutzer per Name entfernen (Debug)\n"
                    "Ihre Auswahl: ")
 
     if action == '1':
 
-        user = input("name: ")
-        if user == '' or user == ' ':
-            print("abgebrochen")
+        user = input("name: ").strip()  #leerzeichen werden entfernt
+        if user == '':
+            print("da muss schon was stehen")
             continue
-        password = input("passwort: ")
+        password = input("passwort: ").strip()  #leerzeichen werden entfernt
         if password == '' or password == ' ':
             print("abgebrochen")
             continue
 
-        BothIsKnown = any(user.lower() == existing_user.lower() and password == existing_password for (existing_user, existing_password) in all_users)
+        BothIsKnown = any(user.lower() == existing_user[1].lower() and password == existing_user[5] for
+                        existing_user in all_users)
 
         if BothIsKnown:
             print("erfolgreich eingeloggt")
-            logged_in = True
+            # logged_in = True
         else:
             print("benutzername oder passwort ist falsch")
 
     elif action == '2':
         while True:
-            user = input("name: ")
+            user = input("name: ").strip()
 
-            if user == '' or user == ' ':
-                print("abgebrochen")
+            if user == '' or len(user) < 3 or len(user) > 16:
+                print("Benutzername muss zwischen 6 und 16 zeichen sein")
                 break
 
-            OneIsKnown = any(user.lower() == existing_user.lower() for (existing_user, _) in all_users)
+            OneIsKnown = any(user.lower() == existing_user[1].lower() for existing_user in all_users)
             if OneIsKnown:
                 print("Benutzer bereits vergeben")
                 continue
             password = input("passwort: ")
-            if password == '' or password == ' ':
-                print("abgebrochen")
+            if password == '' or len(password) < 6 or len(password) > 16:
+                print("passwort muss zwischen 6 und 16 zeichen sein")
                 break
-            addUser(user, password)
+            add_user(user, password)
             print("benutzer erfogreich hinzugefügt")
             break
+
     elif action == '3':
         all_users = get_all_data()
         print(all_users)
+
+    elif action == '4':
+        user_to_delete = input("Der zu löschende Name: ")
+        if  any(user_to_delete.lower() == existing_user[1].lower() for existing_user in all_users):
+            delete_user(user_to_delete)
+            print("Nutzer erfolgreich entfernt")
+        else:
+            print("Nutzer nicht gefunden")
     else:
+        my_db.close()
         break
