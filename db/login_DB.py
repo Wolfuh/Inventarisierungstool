c7bcfd26c1dc433646242868221421fc34800987
Tobias Born <Tobias.Born@srhk.de>
2024-11-08 13:18:58 +0100

    überarbeite login_DB.py

--- db/login_DB.py
+++ db/login_DB.py
@@ -2,16 +2,16 @@ import sqlite3
 from argon2 import PasswordHasher
 
 ph = PasswordHasher()
-def login_lookup(username: str, password: str):
-# Überprüfung der Anmeldedaten und gibt Erfolgsmeldung zurück
 
-    my_db = sqlite3.connect('./db/users.db')    # Verbindung DB
-    my_dbc = my_db.cursor()                     # Cursor erstellen
-    my_dbc.execute("SELECT password_hash FROM users WHERE user_first_name = '" + username + "'") # Hash des Passwortes des eingebenen Nutzers abfragen
-    password_hash = my_dbc.fetchone()
+def login_lookup(username: str, password: str):
+    # Überprüfung der Anmeldedaten, gibt Erfolgsmeldung zurück
+    users_db = sqlite3.connect('./db/users.db') # Verbindung DB
+    users_dbc = users_db.cursor()               # Cursor erstellen
+    users_dbc.execute(f"SELECT password_hash FROM users WHERE user_first_name = '{username}'") # Hash des Passwortes des eingebenen Nutzers abfragen
+    password_hash = users_dbc.fetchone()
 
     try:
-        ph.verify(password_hash[0], password)     # Überprüfung, ob Passwort und Benutzername übereinstimmen
+        ph.verify(password_hash[0], password)   # Überprüfung, ob Passwort und Benutzername übereinstimmen
         return True
     except:
         return False
