152a586b4a949969b75cda47f8edbacd183fea20
Tobias Born <Tobias.Born@srhk.de>
2024-11-08 11:55:31 +0100

    Behebe Fehler, durch den keine Fehlermeldung bei Anmeldeversuchen mit falschen Anmeldedaten angezeigt wird

--- db/login_DB.py
+++ db/login_DB.py
@@ -10,7 +10,8 @@ def login_lookup(username: str, password: str):
     my_dbc.execute("SELECT password_hash FROM users WHERE user_first_name = '" + username + "'") # Hash des Passwortes des eingebenen Nutzers abfragen
     password_hash = my_dbc.fetchone()
 
-    if ph.verify(password_hash[0], password):     # Überprüfung, ob Passwort und Benutzername übereinstimmen
+    try:
+        ph.verify(password_hash[0], password)     # Überprüfung, ob Passwort und Benutzername übereinstimmen
         return True
-    else:
+    except:
         return False
