f147f706a9d7d81715ebfa46a1b6bedfed26da6e
MrRuheeeee <mathishenkel@gmail.com>
2024-12-09 11:28:33 +0100

    DB Datei Kommentiert + Nutzer hinzufügen def hinzugefügt

--- db/SQLite_db.py
+++ db/SQLite_db.py
@@ -5,26 +5,16 @@ import hashlib
 
 username_global = "Benutzername Ungültig"
 
-def init_connection():
-    """
-    Hilfsfunktion zur Herstellung einer Verbindung mit der SQLite-Datenbank.
-    - Die Datenbankdatei muss unter dem angegebenen Pfad existieren.
-    - row_factory wird auf sqlite3.Row gesetzt, um die Ergebnisse als Dictionaries zurückzugeben.
-    """
-    path:str = os.path.join(os.path.dirname(__file__), 'Inventarisierungs_DB.sqlite3')      #+'./db/Inventarisierungs_DB.sqlite3'
+def init_connection():      # verbindung mit DB erstellen und Variable der Verbindung geben
+    path:str = os.path.join(os.path.dirname(__file__), 'Inventarisierungs_DB.sqlite3')    # DB weg, der mit VS Code und PyCharm erreichbar ist
     my_db = sqlite3.connect(path)
-    # Wichtig ist das hier der Root-Pfad angegeben wirddaadvjrnjrmgkmvkmvlddmvlmvk,vfg fifmvlf ,gr,or,vorr,ogl
-
     return my_db
 
-def fetch_items():
-    """
-    - Gibt eine Liste von items zurück
-    - Gibt Spaltenname zu allem mit
-    """
+
+def fetch_items():  # Gibt die items aus der items DB zurück
     try:
         my_db = init_connection()
-        cur = my_db.cursor()
+        cur = my_db.cursor()       # cur für cursor, welcher SQLite funktionen ausführen kann
         cur.execute("SELECT * FROM items")
         items_rows = cur.fetchall()      
         return items_rows
@@ -32,16 +22,19 @@ def fetch_items():
         return [], "Fehler beim Abrufen der Hardware-Einträge:", str(e)
     finally:
         if my_db:
-            my_db.close()
+            my_db.close()   # egal was passiert, die DB wird Geschlossen
 
-def fetch_items_headers():
+
+def fetch_items_headers():  # Gibt die Überschriften der items DB zurück, ohne die Anzahl zu kennen
     try:
         my_db = init_connection()
         cur = my_db.cursor()
-        cur.execute("PRAGMA table_info(items)")
+        cur.execute("PRAGMA table_info(items)") # sucht die Überschriften
         it_alles = cur.fetchall()
-        entfernungs_liste = ["product_id", "added_by_user"]     # blendet die übeschriften aus
-        items_uberschrift = [i[1] for i in it_alles if i[1].lower() not in entfernungs_liste]
+        entfernungs_liste = ["product_id", "added_by_user"]     # blendet die Übeschriften aus (nur hier veränderbar)
+        items_uberschrift = [i[1] for i in it_alles if i[1].lower() not in entfernungs_liste]   
+        # überprüft, ob weitere Überschriften da sind und wenn ja, dann wird diese kleingeschrieben zurück gegeben
+
         return items_uberschrift
     except sqlite3.Error as e:
         return [], "Fehler beim Abrufen der Hardware-Einträge:", str(e)
@@ -49,7 +42,8 @@ def fetch_items_headers():
         if my_db:
             my_db.close()
 
-def fetch_users():
+
+def fetch_users():  # Gibt die Benutzertabelle wieder
     try:
         my_db = init_connection()
         cur = my_db.cursor()
@@ -62,7 +56,8 @@ def fetch_users():
         if my_db:
             my_db.close()
 
-def fetch_users_headers():
+
+def fetch_users_headers():  # Gibt die Überschriften der Benutzertabelle wieder
     try:
         my_db = init_connection()
         cur = my_db.cursor()
@@ -75,56 +70,50 @@ def fetch_users_headers():
     finally:
         if my_db:
             my_db.close()
-def login_lookup(username: str, password: str):
-    
-    global username_global
-    my_db = init_connection() # Verbindung DB
-    cur = my_db.cursor()               # Cursor erstellen
-    cur.execute(f"SELECT passwort FROM benutzer WHERE Benutzername = '{username}'") # Hash des Passwortes des eingebenen Nutzers abfragen
-    back_password = cur.fetchone()
-    username_global = username
-    hash_password = hashlib.sha512(password.encode()).hexdigest()
-    
-    my_db.close()
 
+
+def login_lookup(username: str, password: str): # überprüfung eingehendes Passwort und Benutzername    
     try:
-        if hash_password == back_password[0]:
+        global username_global    # Variable soll außerhalb der Definition nutzbar sein
+        my_db = init_connection() # Verbindung DB
+        cur = my_db.cursor()      # Cursor erstellen
+        cur.execute(f"SELECT passwort FROM benutzer WHERE Benutzername = '{username}'")         
+        back_password = cur.fetchone()
+        # gibt das Passwort des eingegebenen Benutzers wieder (bereits gehashed), falls dieser Existiert
+
+        username_global = username  # Globaler Benutzername auf die Benutzername Eingabe Gesetzt
+        hash_password = hashlib.sha512(password.encode()).hexdigest() # Das eingegebene Passwort hashen
+        if hash_password == back_password[0]: # überprüfung ob das gehashte passwort von der Eingabe, mit dem aus der DB übereinstimmt 
             return True
         else:
-            username_global = ""
-        
+            username_global = ""  # Globalen Benutzernamen leeren
     except:
         username_global = ""
         return False
+    finally:
+        if my_db:
+            my_db.close()
 
-def delete_user():
-    global username_global
-    username_global = ""
 
 
-def lookup_user_stuff():
-    
-    my_db = init_connection() # Verbindung DB
-    cur = my_db.cursor()               # Cursor erstellen
-    cur.execute(f"SELECT Rolle FROM benutzer WHERE Benutzername = '{username_global}'") 
-    role = cur.fetchone()
-    cur.execute(f"SELECT Vorname FROM benutzer WHERE Benutzername = '{username_global}'") 
-    first_name = cur.fetchone()
-    cur.execute(f"SELECT Nachname FROM benutzer WHERE Benutzername = '{username_global}'") 
-    last_name = cur.fetchone()
-    cur.execute(f"SELECT Klasse FROM benutzer WHERE Benutzername = '{username_global}'") 
-    class_name = cur.fetchone()
-    my_db.close()
-    try:
+
+
+def lookup_user_stuff(): # Gibt die Nutzerinformationen
+    try:    # Nutzt den Globalen Benutzername um an Informationen zu gelangen
+        my_db = init_connection() # Verbindung DB
+        cur = my_db.cursor()      # Cursor erstellen
+        cur.execute(f"SELECT Rolle FROM benutzer WHERE Benutzername = '{username_global}'") 
+        role = cur.fetchone()
+        cur.execute(f"SELECT Vorname FROM benutzer WHERE Benutzername = '{username_global}'") 
+        first_name = cur.fetchone()
+        cur.execute(f"SELECT Nachname FROM benutzer WHERE Benutzername = '{username_global}'") 
+        last_name = cur.fetchone()
+        cur.execute(f"SELECT Klasse FROM benutzer WHERE Benutzername = '{username_global}'") 
+        class_name = cur.fetchone()
+        my_db.close()    
         return role, first_name, last_name, class_name
     except sqlite3.Error as e:
-        return [], "Fehler beim Abrufen der Rolle:", str(e)
-
-
-
-# login_lookup("Mathis","123456")
-# lookup_role()
-# print(username_global)
+        return [], "Fehler beim Abrufen der Informationen:", str(e)
 
 
 
@@ -140,36 +129,49 @@ def lookup_user_stuff():
 
 
 
-
-def search_bar_update(search): 
-
-    my_db = init_connection()
-    cur = my_db.cursor()
-
-    # Spaltennamen dynamisch abrufen
-    cur.execute(f"PRAGMA table_info(items)")
-    columns = [row[1] for row in cur.fetchall()]  # Spaltennamen extrahieren
-
-    if not columns:
-        return "Die Tabelle hat keine Spalten oder existiert nicht."
-
-    # Dynamische WHERE-Bedingung für alle Spalten erstellen
-    where_clause = " OR ".join([f"{col} LIKE ?" for col in columns])
-
-    # Dynamisches SQL-Query erstellen
-    query = f"SELECT * FROM items WHERE {where_clause}"
-    cur.execute(query, [f"%{search}%"] * len(columns))
-    answer_search = cur.fetchall()
-    return answer_search
+##############################
+## UNBENUTZTE DEFINITIONEN: ##
+##############################
 
 
-# items_uberschrift = fetch_items_headers()
-# sql_uberschrift = ("OR" + items_uberschrift)
+def search_bar_update(search): # Aktualisiert die Sucheingabe für die items Tabelle - muss noch eingefügt werden
+    try:
+        my_db = init_connection()
+        cur = my_db.cursor()
+
+        cur.execute(f"PRAGMA table_info(items)")
+        columns = [row[1] for row in cur.fetchall()]  # Spaltennamen wiedergeben
+
+        if not columns:
+            return "Die Tabelle hat keine Spalten oder existiert nicht."
+
+        # durchsucht alles aus der items Tabelle und gibt die komplette Zeile zurück
+        where_clause = " OR ".join([f"{col} LIKE ?" for col in columns])
+        query = f"SELECT * FROM items WHERE {where_clause}"
+        cur.execute(query, [f"%{search}%"] * len(columns))
+        answer_search = cur.fetchall()
+        return answer_search
+    except:
+        return
 
 
+def add_user(new_username, new_first_name, new_last_name, new_class, new_role):
+    # fügt neuen Benutzer hinzu, benötigt 5 eingaben und überprüft diese
+    try:
+        new_username = new_username.replace(" ", "")
+        new_first_name = new_first_name.replace(" ", "")
+        new_last_name = new_last_name.replace(" ", "")
+        new_class = new_class.replace(" ", "")
+        new_r = new_r.replace(" ", "")
+        my_db = init_connection()
+        cur = my_db.cursor()
 
-
-
+        cur.execute("INSERT INTO benutzer (Benutzername, Vorname, Nachname, Klasse, Rolle) VALUES (?,?,?,?,?)", 
+                                new_username, new_first_name, new_last_name, new_class, new_role)
+        my_db.commit() # führt die funktion aus
+        my_db.close()   
+    except:
+        return False
 
 
 
@@ -183,6 +185,14 @@ def search_bar_update(search):
 
 
 
+############### DEBUG STUFF ###############
+# login_lookup("Mathis","123456")
+# lookup_role()
+# print(username_global)
+# sibllllll = search_bar_update("et")
+# print(sibllllll)
+
+############### ENDE DEBUG ################
 
 
 
@@ -192,13 +202,11 @@ def search_bar_update(search):
 
 
 
-
-
-
-
-
-
-
+#################################
+# def delete_username_global(): #
+#     global username_global    #   eventuell später einfügen
+#     username_global = ""      #
+#################################
 
 
 
