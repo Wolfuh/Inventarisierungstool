b6964c8d8de83ca5d46bb09316cd387a02805c7e
gerkhan <hans.gerk@srhk.de>
2024-12-12 10:12:41 +0100

    fixing last commit

--- db/test.py
+++ db/test.py
@@ -1,2 +1,12 @@
-from SQLite_db import *
-item_update_damage(8,"DMG","test","description")
+# from SQLite_db import *
+# item_update_damage(8,"DMG","test","description")
+
+import hashlib
+
+
+
+password = input("was willst du Hashen? - ")
+hash_password = hashlib.sha512(password.encode()).hexdigest()
+print(hash_password)
+
+# 🐧🐧🐧🐧🐧🐧
